96148aeb5c53384a5aa6af324268d78d1f55c597
duygukucukoglu35 <duygukucukoglu35@gmail.com>
2022-11-25 07:08:44 +0300

    add router path

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { GlobalStyles } from '../components/globalStyles/GlobalStyles'
 import Navbar from "../components/nav/Navbar"
+import About from '../pages/about/About'
+import Detail from '../pages/detail/Detail'
+import Home from '../pages/home/Home'
+import Login from '../pages/login/Login'
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+    <GlobalStyles/>
     <Navbar/>
     <Routes>
-    <Route>
-    </Route>
+    <Route path="/" element={<Home/>}/>
+    <Route path="login" element={<Login/>}/>
+    <Route path="about" element={<About/>}/>
+    <Route path="detail" element={<Detail/>}/>
+
+ 
 
     </Routes>
 
